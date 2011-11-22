5e7ad28b8e413cefcde109347b3310f0b41c9199
holubjan <holubjan@a9a55f08-eb17-a5f7-0fc1-32f5727935bc>
2011-11-22 21:52:34 +0000

    zalohovanie databazy

---
+++ trunk/pilcik/zaloha.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class zaloha
+    Inherits System.Windows.Forms.Form
+
+    'Formulář přepisuje metodu Dispose, aby vyčistil seznam součástí.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Vyžadováno Návrhářem formulářů
+    Private components As System.ComponentModel.IContainer
+
+    'POZNÁMKA: Následující procedura je vyžadována Návrhářem formulářů
+    'Může být upraveno pomocí Návrháře formulářů.  
+    'Neupravovat pomocí editoru kódu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.zalohaButton = New System.Windows.Forms.Button()
+        Me.obnovListBox = New System.Windows.Forms.ListBox()
+        Me.zalohujRadioButton = New System.Windows.Forms.RadioButton()
+        Me.obnovRadioButton = New System.Windows.Forms.RadioButton()
+        Me.zaloha_label = New System.Windows.Forms.Label()
+        Me.obnovButton = New System.Windows.Forms.Button()
+        Me.zmazButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(510, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(270, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Záloha databázy"
+        '
+        'zalohaButton
+        '
+        Me.zalohaButton.Location = New System.Drawing.Point(325, 51)
+        Me.zalohaButton.Name = "zalohaButton"
+        Me.zalohaButton.Size = New System.Drawing.Size(75, 23)
+        Me.zalohaButton.TabIndex = 1
+        Me.zalohaButton.Text = "Zálohuj"
+        Me.zalohaButton.UseVisualStyleBackColor = True
+        '
+        'obnovListBox
+        '
+        Me.obnovListBox.FormattingEnabled = True
+        Me.obnovListBox.Location = New System.Drawing.Point(325, 158)
+        Me.obnovListBox.Name = "obnovListBox"
+        Me.obnovListBox.Size = New System.Drawing.Size(394, 160)
+        Me.obnovListBox.TabIndex = 2
+        '
+        'zalohujRadioButton
+        '
+        Me.zalohujRadioButton.AutoSize = True
+        Me.zalohujRadioButton.Location = New System.Drawing.Point(22, 57)
+        Me.zalohujRadioButton.Name = "zalohujRadioButton"
+        Me.zalohujRadioButton.Size = New System.Drawing.Size(60, 17)
+        Me.zalohujRadioButton.TabIndex = 3
+        Me.zalohujRadioButton.TabStop = True
+        Me.zalohujRadioButton.Text = "Zálohuj"
+        Me.zalohujRadioButton.UseVisualStyleBackColor = True
+        '
+        'obnovRadioButton
+        '
+        Me.obnovRadioButton.AutoSize = True
+        Me.obnovRadioButton.Location = New System.Drawing.Point(22, 215)
+        Me.obnovRadioButton.Name = "obnovRadioButton"
+        Me.obnovRadioButton.Size = New System.Drawing.Size(104, 17)
+        Me.obnovRadioButton.TabIndex = 4
+        Me.obnovRadioButton.TabStop = True
+        Me.obnovRadioButton.Text = "Obnov zo zálohy"
+        Me.obnovRadioButton.UseVisualStyleBackColor = True
+        '
+        'zaloha_label
+        '
+        Me.zaloha_label.AutoSize = True
+        Me.zaloha_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.zaloha_label.Location = New System.Drawing.Point(34, 86)
+        Me.zaloha_label.Name = "zaloha_label"
+        Me.zaloha_label.Size = New System.Drawing.Size(333, 13)
+        Me.zaloha_label.TabIndex = 5
+        Me.zaloha_label.Text = "Zálohu databázy vytvoríte stlačením na tlačítko 'Zálohuj'"
+        '
+        'obnovButton
+        '
+        Me.obnovButton.Location = New System.Drawing.Point(349, 337)
+        Me.obnovButton.Name = "obnovButton"
+        Me.obnovButton.Size = New System.Drawing.Size(75, 23)
+        Me.obnovButton.TabIndex = 6
+        Me.obnovButton.Text = "Obnov"
+        Me.obnovButton.UseVisualStyleBackColor = True
+        '
+        'zmazButton
+        '
+        Me.zmazButton.Location = New System.Drawing.Point(588, 337)
+        Me.zmazButton.Name = "zmazButton"
+        Me.zmazButton.Size = New System.Drawing.Size(75, 23)
+        Me.zmazButton.TabIndex = 7
+        Me.zmazButton.Text = "Zmaž zálohu"
+        Me.zmazButton.UseVisualStyleBackColor = True
+        '
+        'zaloha
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(816, 372)
+        Me.Controls.Add(Me.zmazButton)
+        Me.Controls.Add(Me.obnovButton)
+        Me.Controls.Add(Me.zaloha_label)
+        Me.Controls.Add(Me.obnovRadioButton)
+        Me.Controls.Add(Me.zalohujRadioButton)
+        Me.Controls.Add(Me.obnovListBox)
+        Me.Controls.Add(Me.zalohaButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "zaloha"
+        Me.Text = "zaloha"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents zalohaButton As System.Windows.Forms.Button
+    Friend WithEvents obnovListBox As System.Windows.Forms.ListBox
+    Friend WithEvents zalohujRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents obnovRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents zaloha_label As System.Windows.Forms.Label
+    Friend WithEvents obnovButton As System.Windows.Forms.Button
+    Friend WithEvents zmazButton As System.Windows.Forms.Button
+End Class
---
+++ trunk/pilcik/zaloha.vb
@@ -0,0 +1,185 @@
+﻿Imports System.IO
+Public Class zaloha
+
+    Private Sub zaloha_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = hlavna_aplikacia
+        Me.WindowState = FormWindowState.Maximized
+        Me.BringToFront()
+
+        zalohaButton.Enabled = False
+        obnovButton.Enabled = False
+        zmazButton.Enabled = False
+        obnovListBox.Enabled = False
+
+        'nacitanie zoznamu suborov do listboxu
+        obnovListBox.Items.Clear()
+        Dim cDir As String
+        cDir = CurDir()
+        Dim fileEntries As String()
+        Try
+            fileEntries = Directory.GetFiles(cDir & "\backup", "*.accdb-*")
+
+            Dim fileName As String
+            Dim tempFile As String
+            For Each fileName In fileEntries
+                tempFile = fileName.Remove(0, (cDir & "\backup").Length + 1)
+                obnovListBox.Items.Add(tempFile)
+
+            Next fileName
+        Catch ex As Exception
+            MsgBox("Adresár so zálohami nenájdený. Možná príčina - zatial ste nerobili žiadne zálohy. Prosím spravte zálohu databázy.")
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub zalohaButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zalohaButton.Click
+
+        'zalohuje databzovy subor do priecinka backup
+        Dim cDir As String
+        Dim source As String
+        Dim destination As String
+        Dim now As String
+        cDir = CurDir()
+        now = Format(DateTime.Now, "dd-MM-yyyy--HH-mm-ss")
+
+        source = cDir & "\pilcik_db.accdb"
+        destination = cDir & "\backup\pilcik_db.accdb-" & now
+
+
+        'poznamka.Text = source & "     " & destination
+
+        Try
+            If My.Computer.FileSystem.DirectoryExists(cDir & "\backup") Then
+                My.Computer.FileSystem.CopyFile(source, destination)
+                '       poznamka.Visible = True
+                '      poznamka.Text = "Záloha databázy úspešne vytvorená."
+
+            Else
+                My.Computer.FileSystem.CreateDirectory(cDir & "\backup")
+                My.Computer.FileSystem.CopyFile(source, destination)
+                'poznamka.Visible = True
+                'poznamka.Text = "Záloha databázy úspešne vytvorená."
+
+            End If
+        Catch ex As Exception
+            MsgBox("Nepodarilo sa zálohovať databázu.")
+        End Try
+
+
+        obnovListBox.Items.Clear()
+        
+        Dim fileEntries As String()
+        Try
+            fileEntries = Directory.GetFiles(cDir & "\backup", "*.accdb-*")
+
+            Dim fileName As String
+            Dim tempFile As String
+            For Each fileName In fileEntries
+                tempFile = fileName.Remove(0, (cDir & "\backup").Length + 1)
+                obnovListBox.Items.Add(tempFile)
+
+            Next fileName
+        Catch ex As Exception
+            MsgBox("Adresár so zálohami nenájdený. Možná príčina - zatial ste nerobili žiadne zálohy. Prosím spravte zálohu databázy.")
+        End Try
+
+    End Sub
+
+    Private Sub zalohujRadioButton_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zalohujRadioButton.CheckedChanged
+        If zalohujRadioButton.Checked Then
+            zalohaButton.Enabled = True
+        Else
+            zalohaButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub obnovRadioButton_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles obnovRadioButton.CheckedChanged
+        If obnovRadioButton.Checked Then
+            obnovListBox.Enabled = True
+            obnovButton.Enabled = True
+            zmazButton.Enabled = True
+        Else
+            obnovListBox.Enabled = False
+            obnovButton.Enabled = False
+            zmazButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub zmazButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zmazButton.Click
+        Dim cDir As String
+        Dim source As String
+
+
+        cDir = CurDir()
+
+
+        If Not obnovListBox.SelectedIndex = -1 Then
+
+            source = cDir & "\backup\" & obnovListBox.SelectedItem
+
+
+            'poznamka.Text = source & "     " & destination
+
+            Try
+                My.Computer.FileSystem.DeleteFile(source)
+                'poznamka_obnova.Visible = True
+                'poznamka_obnova.Text = "Vymazanie zálohy databázy úspešné."
+            Catch ex As Exception
+                MsgBox("Nepodarilo sa vymazať zálohu databázy.")
+
+            End Try
+
+        Else
+            MsgBox("Pre vymazanie musíte označiť položku zo zoznamu záloh databázy.")
+        End If
+
+        obnovListBox.Items.Clear()
+
+        Dim fileEntries As String()
+        Try
+            fileEntries = Directory.GetFiles(cDir & "\backup", "*.accdb-*")
+
+            Dim fileName As String
+            Dim tempFile As String
+            For Each fileName In fileEntries
+                tempFile = fileName.Remove(0, (cDir & "\backup").Length + 1)
+                obnovListBox.Items.Add(tempFile)
+
+            Next fileName
+        Catch ex As Exception
+            MsgBox("Adresár so zálohami nenájdený.")
+        End Try
+    End Sub
+
+    Private Sub obnovButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles obnovButton.Click
+        Dim cDir As String
+        Dim source As String
+        Dim destination As String
+
+        cDir = CurDir()
+
+
+        If Not obnovListBox.SelectedIndex = -1 Then
+
+            source = cDir & "\backup\" & obnovListBox.SelectedItem
+            destination = cDir & "\pilcik_db.accdb"
+
+            'poznamka.Text = source & "     " & destination
+
+            Try
+                My.Computer.FileSystem.CopyFile(source, destination, True)
+                'poznamka_obnova.Visible = True
+                'poznamka_obnova.Text = "Obnova databázy úspešná."
+            Catch ex As Exception
+                MsgBox("Nepodarilo sa spraviť obnovu databázy.")
+
+            End Try
+
+        Else
+            MsgBox("Pre obnovu musíte označiť položku zo zoznamu záloh databázy.")
+        End If
+    End Sub
+End Class
